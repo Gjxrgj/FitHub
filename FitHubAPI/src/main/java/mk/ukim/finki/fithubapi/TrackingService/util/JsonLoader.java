b84bfef4ce5a0cdb33284f8f202f2acb9cbcfbe1
Marko_Gjorgjievski_201136 <marko.gjorgjievski@students.finki.ukim.mk>
2025-07-25 23:04:32 +0200

    Updated dependencies for jackson. Made the JsonLoader case-insensitive for enums.

--- FitHubAPI/src/main/java/mk/ukim/finki/fithubapi/TrackingService/util/JsonLoader.java
+++ FitHubAPI/src/main/java/mk/ukim/finki/fithubapi/TrackingService/util/JsonLoader.java
@@ -1,14 +1,16 @@
 package mk.ukim.finki.fithubapi.TrackingService.util;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import mk.ukim.finki.fithubapi.TrackingService.model.Exercise;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
 import java.io.InputStream;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+
 
 @Service
 public class JsonLoader {
@@ -17,6 +19,9 @@ public class JsonLoader {
         ObjectMapper objectMapper = new ObjectMapper();
 
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true);
+
+
 
         try (InputStream inputStream = new ClassPathResource("exercises.json").getInputStream()) {
             return objectMapper.readValue(
